064ceefb7612b1931816fa981473d1874ad71d29
nshcode <103318915+nshcode@users.noreply.github.com>
2025-05-27 22:34:16 +0200

    Converting a JSON collection into a relational table

--- laureates/json-collection2relational-table.sql
+++ laureates/json-collection2relational-table.sql
@@ -7,19 +7,19 @@ SELECT
 FROM laureates, 
   JSON_TABLE(laureate, '$'
     COLUMNS (
-      f_name          VARCHAR2 PATH '$.firstname'
-      ,l_name         VARCHAR2 PATH '$.surname'
-      ,gender         VARCHAR2 PATH  '$.gender'
+      first_name      VARCHAR2 PATH '$.firstname'
+      ,last_name      VARCHAR2 PATH '$.surname'
+      ,gender         VARCHAR2 PATH '$.gender'
       ,date_of_birth  DATE     PATH '$.born'
       ,date_of_death  DATE     PATH '$.died'
-      ,born_in        VARCHAR2 PATH  '$.bornCountry'
-      ,died_in        VARCHAR2 PATH  '$.diedCountry'
+      ,born_in        VARCHAR2 PATH '$.bornCountry'
+      ,died_in        VARCHAR2 PATH '$.diedCountry'
       ,NESTED PATH '$.prizes[*]'
             COLUMNS (
               category    VARCHAR2 PATH '$.category'
-              ,year       INTEGER  PATH  '$.year'
+              ,year       INTEGER  PATH '$.year'
               ,motivation VARCHAR2 PATH '$.motivation'
-              ,NESTED PATH          '$.affiliations[*]'
+              ,NESTED PATH              '$.affiliations[*]'
                     COLUMNS (
                       affiliation         VARCHAR2  PATH '$.name'
                       ,affiliation_city   VARCHAR2  PATH '$.city'
